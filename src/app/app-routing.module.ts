b6fb3dc6ad3f567af4c1c01ff6f28c2a8e5ea388
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2020-10-24 17:13:29 -0400

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'editor',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
+    path: 'editor',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   }
 ];
